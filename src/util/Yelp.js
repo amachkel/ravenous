29b4ced46de7cfbef2ad61d0e95fd94a80239d45
amachkel <amachkel@gmail.com>
2022-05-10 20:05:04 -0400

    pull data from yelp api and render to screen

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey =
+  "8Pf7vZRVyEwXfYGxtySYgxmcYjhGBchBjt91WdP-Xt2WO7O77jb7SFBibKJsafBWOeEfSwlBSyxdJgetUUjkHmjqbLYBVdOFisa32GUQxtjiLxtAQPq15eOJnO96YnYx";
+
+const Yelp = {
+  async search(term, location, sortBy) {
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map((business) => ({
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.location.address1,
+        city: business.location.city,
+        state: business.location.state,
+        zipCode: business.location.zip_code,
+        category: business.categories[0].title,
+        rating: business.rating,
+        reviewCount: business.review_count,
+      }));
+    }
+  },
+};
+
+export default Yelp;
